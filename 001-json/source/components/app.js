24e4fdced04de2ffc648b60958766f5acfe1d39a
Ricky Do <ricky.dop@gmail.com>
2018-02-09 07:49:16 -0600

    update gitignore and finished 001

--- 001-json/source/components/app.js
+++ 001-json/source/components/app.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 export default class App extends React.Component {
-	render(){
-		var smartMessage = this.props.smart? "He is smart.":"He is not smart";
+	render() {
+		var isSmart = this.props.smart? "He is smart" : "He is not smart";
+		return (
+			<div>
+				<h1>{this.props.name}</h1>
+				<p>He is at least {this.props.age} years old.</p>
+				<p>{isSmart}</p>
+			</div>
 
-		return <div>
-					<h1>{this.props.name}</h1>
-					<p>He is at least {this.props.age} years old. {smartMessage}</p>
-
-		</div>;
+		)
 	}
-
 }
